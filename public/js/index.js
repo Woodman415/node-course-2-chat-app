333bf79852d45287bbc470040d82f694697bd24e
Duane Wood <duane.wood.yyc@gmail.com>
2019-06-26 14:19:30 -0600

    Added event listener

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@
   socket.on('connect', function () {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-      from: 'Duane',
-      text: 'Hey, whats up?'
-    });
   });
 
   socket.on('disconnect', function () {
