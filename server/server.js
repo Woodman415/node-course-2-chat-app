27479d6ad3b84736f6fc07e8c88f575edc5812f1
Duane Wood <duane.wood.yyc@gmail.com>
2019-06-26 13:47:49 -0600

    Add new message and create message events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'See ya!',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnet', () => {
     console.log('User discconnected');
   });
