314fe1cf5be6888ad4e6feadac4b1458f9fa6c55
Duane Wood <duane.wood.yyc@gmail.com>
2019-06-27 10:40:16 -0600

    Added geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -12,3 +12,16 @@ describe('generateMessage', () => {
     expect(message).toInclude({from, text});
   });
 });
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location message object', () => {
+    var from = 'Jen';
+    var latitude = 10;
+    var longitude = 20;
+    var url = 'https://www.google.com/maps?q=10,20';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
+  });
+});
